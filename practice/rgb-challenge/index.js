534924545712055cd9591a2f2e38dbe51836b121
underbleu <underbleu.dev@gmail.com>
2017-12-20 10:27:17 +0900

    RGB 챌린지 프로젝트 자원

---
+++ practice/rgb-challenge/index.js
@@ -0,0 +1,13 @@
+function random255(){
+  return Math.floor(Math.random() * 256);
+}
+
+function randomColor(){
+  return `rgb(${random255()}, ${random255()}, ${random255()})`
+}
+
+document.querySelectorAll('.box').forEach(el => {
+  el.style.backgroundColor = randomColor();
+})
+// 정답
+
