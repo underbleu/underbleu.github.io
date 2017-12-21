d664c4f2ad84e1c5a7c96b711190bb7e9ccd2860
underbleu <underbleu.dev@gmail.com>
2017-12-21 16:48:03 +0900

    RGB 챌린지__ 다음단계로 전환까지 완성 (중복코드 많은 상태)

--- practice/rgb-challenge/index.js
+++ practice/rgb-challenge/index.js
@@ -6,8 +6,39 @@ function randomColor(){
   return `rgb(${random255()}, ${random255()}, ${random255()})`
 }
 
-document.querySelectorAll('.box').forEach(el => {
-  el.style.backgroundColor = randomColor();
+// document.querySelectorAll('.box').forEach(el => {
+//   el.style.backgroundColor = randomColor();
+// })
+
+let stage = 0;
+let problem = [randomColor(), randomColor(), randomColor()];
+let correctAnswer = 0;
+
+// nodelist.forEach 사용
+document.querySelectorAll('.box').forEach((el, index) => {
+  el.style.backgroundColor = problem[index];
+  el.addEventListener('click', e => {
+    if(index === correctAnswer){
+      // 다음단계로 전환
+      stage++;
+      problem = [randomColor(), randomColor(), randomColor()];
+      // 화면 다시그리기
+      document.querySelectorAll('.box').forEach((el, index) => {
+        el.style.backgroundColor = problem[index];
+      })
+
+      document.querySelector('.rgb-text').textContent = problem[correctAnswer];
+    }else{
+      //재시작
+
+    }
+  });
 })
-// 정답
+
+document.querySelector('.rgb-text').textContent = problem[correctAnswer];
+document.querySelector('.rgb-score').textContent = stage;
+
+
+
+
 
