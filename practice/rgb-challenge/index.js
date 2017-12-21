b5b90790e63374f21415428d27972a109ee1c751
underbleu <underbleu.dev@gmail.com>
2017-12-21 16:51:45 +0900

    RGB 챌린지__ 다음단계로 전환까지 완성 (중복코드 -> 함수로 묶어줌)

--- practice/rgb-challenge/index.js
+++ practice/rgb-challenge/index.js
@@ -14,30 +14,32 @@ let stage = 0;
 let problem = [randomColor(), randomColor(), randomColor()];
 let correctAnswer = 0;
 
-// nodelist.forEach 사용
+function nextStage(){
+  // 다음단계로 전환
+  stage++;
+  problem = [randomColor(), randomColor(), randomColor()];
+}
+
+function draw(){
+  // 화면 그리기
+  document.querySelectorAll('.box').forEach((el, index) => {
+    el.style.backgroundColor = problem[index];
+  })
+  document.querySelector('.rgb-text').textContent = problem[correctAnswer];
+}
+
 document.querySelectorAll('.box').forEach((el, index) => {
-  el.style.backgroundColor = problem[index];
   el.addEventListener('click', e => {
     if(index === correctAnswer){
-      // 다음단계로 전환
-      stage++;
-      problem = [randomColor(), randomColor(), randomColor()];
-      // 화면 다시그리기
-      document.querySelectorAll('.box').forEach((el, index) => {
-        el.style.backgroundColor = problem[index];
-      })
-
-      document.querySelector('.rgb-text').textContent = problem[correctAnswer];
+      nextStage();
+      draw();
     }else{
       //재시작
-
     }
   });
 })
 
-document.querySelector('.rgb-text').textContent = problem[correctAnswer];
-document.querySelector('.rgb-score').textContent = stage;
-
+draw();
 
 
 
